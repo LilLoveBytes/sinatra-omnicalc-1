21b5668008d1ae6855f773ae0fb1b2041e7bfe31
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-29 18:15:46 +0000

    hom.erb and routes placeholders

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:home)
+end
+get("/square/new") do
+ erb(:home)
+end
+get("/square_root/new") do
+ erb(:home)
+end
+get("/random/new") do
+ erb(:home)
+end
+get("/payment/new") do
+ erb(:home)
 end
