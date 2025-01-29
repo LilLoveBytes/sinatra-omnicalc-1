f6ec877129806b9bc13ae1237f1a29e281f21a5a
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-29 18:27:51 +0000

    square/new home.erb

--- app.rb
+++ app.rb
@@ -4,15 +4,35 @@ require "sinatra/reloader"
 get("/") do
  erb(:home)
 end
+
 get("/square/new") do
  erb(:home)
 end
+
+get("/square/results") do
+ erb(:results)
+end
+
 get("/square_root/new") do
  erb(:home)
 end
+
+get("/square_root/results") do
+ erb(:home)
+end
+
 get("/random/new") do
  erb(:home)
 end
+
+get("/random/results") do
+ erb(:home)
+end
+
 get("/payment/new") do
+ erb(:home)
+end
+
+get("/payment/results") do
  erb(:home)
 end
